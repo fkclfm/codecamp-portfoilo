44ef4721120394482a734c12cbefed5173ecc8e8
Rogue <fkclfm@naver.com>
2024-11-14 04:25:06 +0900

    UI 리메이크 작업

--- src/units/boards/detail/BoardDetail.presenter.tsx
+++ src/units/boards/detail/BoardDetail.presenter.tsx
@@ -3,78 +3,116 @@ import * as W from "./BoardDetail.styled"; // 스타일을 W로 한번에 임포
 import ReactPlayer from "react-player";
 import { Tooltip } from "antd";
 import { getDate } from "../../../commons/year";
+import Image from "next/image";
 
 export default function BoardDetailUI(props: IBoardDetailProps) {
   return (
-    <W.Entire>
-      <W.Wrapper>
-        <W.Header>
-          <W.ProfileBox>
-            <W.Writer>작성자 : {props.data?.fetchBoard.writer}</W.Writer>
-            <W.Date>Date : {getDate(props.data?.fetchBoard.createdAt)}</W.Date>
-          </W.ProfileBox>
-          <W.AreaBox>
-            <W.location src="/images/ic_link.png" />
-            <Tooltip
-              placement="topRight"
-              title={`${props.data?.fetchBoard.boardAddress?.address ?? ""} 
-              ${props.data?.fetchBoard.boardAddress?.addressDetail ?? ""}`}
-            >
-              <W.location src="/images/ic_location.png" />
-            </Tooltip>
-          </W.AreaBox>
-        </W.Header>
-        <W.HorizonLine />
-        {/* 글 제목 */}
-        <W.TitleText>{props.data?.fetchBoard.title}</W.TitleText>
-        {/* 글 내용 */}
-        {props.data?.fetchBoard.contents}
-        <W.ImageBox>
-          {props.data?.fetchBoard.images
-            ?.filter((el: string) => el !== "")
-            .map((el, index) => (
-              <W.Image key={el} src={`https://storage.googleapis.com/${el}`} />
-            ))}
-        </W.ImageBox>
-        <W.YoutubeBox>
-          <ReactPlayer
-            url={String(props.data?.fetchBoard.youtubeUrl)}
-            width={486}
-            height={240}
-            playing={true}
-            muted={true}
-          />
-        </W.YoutubeBox>
-        <W.RatingBox>
-          <W.LikeBox>
-            <img
-              src="/images/ic_thumb_up.png"
-              alt="like"
-              onClick={props.onClickLike}
+    <W.Wrapper>
+      <W.ImageBox>
+        {props.data?.fetchBoard.images
+          ?.filter((el: string) => el !== "")
+          .map((el, index) => (
+            <Image
+              width={500}
+              height={450}
+              key={el}
+              src={`https://storage.googleapis.com/${el}`}
             />
-            <span>{props.data?.fetchBoard.likeCount}</span>
-          </W.LikeBox>
-          <W.DisLikeBox>
-            <img
-              src="/images/ic_thumb_down.png"
-              alt="dislike"
+          ))}
+      </W.ImageBox>
+      <W.ProfileBox>
+        <W.TitleText>{props.data?.fetchBoard.title}</W.TitleText>
+        <W.UserBox>
+          <W.Writer>{props.data?.fetchBoard.writer}</W.Writer>
+          <W.Date>Date : {getDate(props.data?.fetchBoard.createdAt)}</W.Date>
+        </W.UserBox>
+      </W.ProfileBox>
+      <W.AreaBox>
+        <W.location src="/images/ic_link.png" />
+        <Tooltip
+          placement="topRight"
+          title={`${props.data?.fetchBoard.boardAddress?.address ?? ""} 
+              ${props.data?.fetchBoard.boardAddress?.addressDetail ?? ""}`}
+        >
+          <W.location src="/images/ic_location.png" />
+        </Tooltip>
+      </W.AreaBox>
+      <W.TextBox>
+        {/* 글 내용 */}
+        <W.Text>{props.data?.fetchBoard.contents}</W.Text>
+      </W.TextBox>
+      <W.YoutubeBox>
+        <ReactPlayer
+          url={String(props.data?.fetchBoard.youtubeUrl)}
+          width={486}
+          height={240}
+          playing={true}
+          muted={true}
+        />
+      </W.YoutubeBox>
+      <W.RatingBox>
+        <W.LikeBox>
+          <Image
+            src="/images/ic_thumb_up.png"
+            width={24}
+            height={24}
+            alt="like"
+            onClick={props.onClickLike}
+          />
+          <span>{props.data?.fetchBoard.likeCount}</span>
+        </W.LikeBox>
+        <W.DisLikeBox>
+          <Image
+            src="/images/ic_thumb_down.png"
+            width={24}
+            height={24}
+            alt="dislike"
+            onClick={props.onClickDisLike}
+          />
+          <span>{props.data?.fetchBoard.dislikeCount}</span>
+        </W.DisLikeBox>
+      </W.RatingBox>
+      <W.BtnBox>
+        <W.BoardBtn onClick={props.onClickList}>
+          <W.IconBtn>
+            <Image
+              src="/images/menu.svg"
+              width={22}
+              height={22}
+              alt="menu"
               onClick={props.onClickDisLike}
             />
-            <span>{props.data?.fetchBoard.dislikeCount}</span>
-          </W.DisLikeBox>
-        </W.RatingBox>
-      </W.Wrapper>
-      <W.BtnBox>
-        <W.BoardBtn onClick={props.onClickList}>목록으로</W.BoardBtn>
-        <W.BoardBtn onClick={props.onClickEdit}>수정하기</W.BoardBtn>
+            목록으로
+          </W.IconBtn>
+        </W.BoardBtn>
+        <W.BoardBtn onClick={props.onClickEdit}>
+          <W.IconBtn>
+            <Image
+              src="/images/edit.svg"
+              width={22}
+              height={22}
+              alt="edit"
+              onClick={props.onClickDisLike}
+            />
+            수정하기
+          </W.IconBtn>
+        </W.BoardBtn>
         <W.BoardBtn
           id={props.data?.fetchBoard._id}
           onClick={props.onClickDelete}
         >
-          삭제하기
+          <W.IconBtn>
+            <Image
+              src="/images/delete.svg"
+              width={22}
+              height={22}
+              alt="delete"
+              onClick={props.onClickDisLike}
+            />
+            삭제하기
+          </W.IconBtn>
         </W.BoardBtn>
       </W.BtnBox>
-      <W.HorizonLine />
-    </W.Entire>
+    </W.Wrapper>
   );
 }
--- src/units/boards/detail/BoardDetail.styled.ts
+++ src/units/boards/detail/BoardDetail.styled.ts
@@ -1,45 +1,43 @@
 import styled from "@emotion/styled";
 
-export const Entire = styled.div``;
-
 export const Wrapper = styled.div`
   width: 1200px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 1px solid gray;
   margin: 60px auto;
-  padding: 50px 80px;
+  padding: 50px 40px;
   gap: 15px;
   text-align: center;
 `;
-export const Header = styled.div`
+
+export const ImageBox = styled.div`
   display: flex;
   flex-direction: row;
-  width: 100%;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  margin-top: 45px;
+  gap: 15px;
+  margin-bottom: 30px;
 `;
 
-export const Writer = styled.text`
-  font-size: 24px;
+export const TitleText = styled.span`
+  font-size: 39px;
   font-weight: 500;
   text-align: left;
 `;
 
-export const ProfileBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
+export const Date = styled.span`
+  font-size: 18px;
+  font-weight: 500;
+  color: #828282;
 `;
 
-export const Date = styled.text`
-  font-size: 16px;
-  font-weight: 400;
-  color: #828282;
+export const ProfileBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 export const HorizonLine = styled.div`
@@ -49,34 +47,55 @@ export const HorizonLine = styled.div`
   margin-bottom: 60px;
 `;
 
-export const TitleText = styled.h1`
-  font-size: 36px;
-  text-align: left;
-  padding-bottom: 400px;
+export const TextBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const UserBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 10px;
+`;
+
+export const Writer = styled.h1`
+  font-size: 18px;
+`;
+
+export const Text = styled.span`
+  font-size: 18px;
 `;
 
 export const BtnBox = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   gap: 20px;
-  margin-bottom: 100px;
+  margin-bottom: 50px;
+`;
+
+export const IconBtn = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
 `;
 
 export const BoardBtn = styled.button`
   border: 1px solid black;
-  width: 179px;
+  width: 33%;
   font-size: 16px;
-  font-weight: 500;
-  font-family: "NotoSansKR";
-  background-color: yellow;
+  font-weight: 700;
+  background-color: white;
   height: 45px;
-  opacity: 0.5;
-
-  &:hover {
-    opacity: 1;
-  }
+  cursor: pointer;
 `;
 
 export const RatingBox = styled.div`
@@ -85,6 +104,7 @@ export const RatingBox = styled.div`
   justify-content: center;
   align-items: center;
   gap: 5rem;
+  margin-bottom: 30px;
 `;
 
 export const LikeBox = styled.div`
@@ -116,18 +136,4 @@ export const AreaBox = styled.div`
 export const location = styled.img`
   width: 32px;
   height: 32px;
-`;
-
-export const ImageBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 15px;
-  margin-bottom: 30px;
-`;
-
-export const Image = styled.img`
-  width: 450px;
-  height: 450px;
 `;
